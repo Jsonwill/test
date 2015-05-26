a02fc2d25f34a254af5f94b8d218c4eeb267acb3
Jsonwill <331971498@163.com>
2015-05-26 10:34:57 +0800

    F

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 echo '例子：';
 
-file_put_contents('log.txt', '生存还是毁灭,这是个问题.');
+file_put_contents('/home/xiaoju/webroot/webroot/test/log.txt', '生存还是毁灭,这是个问题.');
 fastcgi_finish_request(); /* 响应完成, 关闭连接 */
  
 /* 记录日志 */
-file_put_contents('log.txt', '生存还是毁灭,这是个问题.');
+file_put_contents('/home/xiaoju/webroot/webroot/test/log1.txt'', '生存还是毁灭,这是个问题.');
 ?>
